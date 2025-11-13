335075894972902191e758a6646e9d68c414a73f
Seba <sebatokoksito@gmail.com>
2025-11-13 14:05:45 +0100

    End of section 15

---
+++ API/Extensions/MessageExtensions.cs
@@ -0,0 +1,43 @@
+using System;
+using API.Entities;
+using API.DTOs;
+using System.Linq.Expressions;
+
+namespace API.Extensions;
+
+public static class MessageExtensions
+{
+    public static MessageDto ToDto(this Message message)
+    {
+        return new MessageDto
+        {
+            Id = message.Id,
+            SenderId = message.SenderId,
+            SenderDisplayName = message.Sender.DisplayName,
+            SenderImageUrl = message.Sender.ImageUrl,
+            RecipientId = message.RecipientId,
+            RecipientDisplayName = message.Recipient.DisplayName,
+            RecipientImageUrl = message.Recipient.ImageUrl,
+            Content = message.Content,
+            DateRead = message.DateRead,
+            MessageSent = message.MessageSent
+        };
+    }
+
+    public static Expression<Func<Message, MessageDto>> ToDtoProjection()
+    {
+        return message => new MessageDto
+        {
+            Id = message.Id,
+            SenderId = message.SenderId,
+            SenderDisplayName = message.Sender.DisplayName,
+            SenderImageUrl = message.Sender.ImageUrl,
+            RecipientId = message.RecipientId,
+            RecipientDisplayName = message.Recipient.DisplayName,
+            RecipientImageUrl = message.Recipient.ImageUrl,
+            Content = message.Content,
+            DateRead = message.DateRead,
+            MessageSent = message.MessageSent
+        };
+    }
+}
